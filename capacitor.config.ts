29079ff77cc4ec47b11dc59ff6109a98eb5930ec
JoseGrass <122506998+JoseGrass@users.noreply.github.com>
2025-05-25 20:16:52 +0000

    cammit4

---
+++ capacitor.config.ts
@@ -0,0 +1,11 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config = {
+  appId: 'com.pokeapi3.app',
+  appName: 'pokeapi3',
+  webDir: 'dist', // o 'proyecto' si ahí está el index.html directamente
+  bundledWebRuntime: false
+};
+
+export default config;
+
